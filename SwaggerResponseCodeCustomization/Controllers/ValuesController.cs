8a99c383d7414a87675a3af2a098e4bace43fe2b
brady gaster <brady.gaster@gmail.com>
2015-06-09 09:54:45 -0700

    initial creation

---
+++ SwaggerResponseCodeCustomization/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SwaggerResponseCodeCustomization.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private IEnumerable<Contact> GetContacts()
+        {
+            return new Contact[]{
+                new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
+                new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
+                new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"}
+            };
+        }
+
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [ResponseType(typeof(Contact))]
+        public HttpResponseMessage Get(int id)
+        {
+            var contacts = GetContacts();
+
+            var requestedContact = contacts.FirstOrDefault(x => x.Id == id);
+
+            if (requestedContact == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                return Request.CreateResponse<Contact>(HttpStatusCode.OK, requestedContact);
+            }
+        }
+    }
+
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ SwashbuckleResponseCodeCustomization.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwashbuckleResponseCodeCustomization.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var response = new SwaggerResponseCodeCustomization().Values.Get(3);
+        }
+    }
+}
